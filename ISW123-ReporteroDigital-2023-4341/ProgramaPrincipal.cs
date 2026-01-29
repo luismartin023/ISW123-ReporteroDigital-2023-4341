856f6c5f1f2038671afcbb31b056b667525924e2
Luis Martin Peña Mejia <luismartinpm2580@gmail.com>
2026-01-28 22:49:21 -0400

    CREACION DE EVENTOS DE DESCARGA Y USO DE THROW NEW PARA SIMULAR UNA "ESTADISTICA", LA VERDA NI YO LO ENTENDI PERO FUNCIONA Y NO DA ERROR

--- ISW123-ReporteroDigital-2023-4341/ProgramaPrincipal.cs
+++ ISW123-ReporteroDigital-2023-4341/ProgramaPrincipal.cs
@@ -30,9 +30,58 @@ namespace ISW123_ReporteroDigital_2023_4341
 
         //Invocacion de eventos y await
 
+        public async Task<string> DescargarTextoAsycn()
+        {
 
+            OnIniciando?.Invoke("Tamo decargando la vaina");
+
+            Console.WriteLine("Descargando...");
+            await Task.Delay(2000); 
+
+            OnFinalizado?.Invoke("Pa, ya se decargo.");
+            return "Texto descargado.";
+
+        }
+
+        // Simulando la descarga de una imagen de manera asincrona
+
+        public async Task<string> DescargarImagenAsync()
+        {
+            OnIniciando?.Invoke("Lider, yo se que tu wifi ta lento pero decarga esto...");
+
+            Console.WriteLine("Descargando imagen...");
+            await Task.Delay(3000); 
+
+            OnFinalizado?.Invoke("Descarga de imagen nitida.");
+            return "[Foto_Goku.jpg, Foto_Naruto.png]";
+        }
+
+        // Datos de visitas de noticias
+
+        public async Task<string> DescargarEstadisticasAsycn()
+        {
+            OnIniciando?.Invoke("Los Datos de Analisis...");
+
+            Console.WriteLine("Descargando estadisticas...");
+            await Task.Delay(1500);
+
+            // Uso de excepcion personalizada throw new
+
+            Random rnd = new Random();
+            if (rnd.Next(0, 2) == 0) 
+            {
+                throw new ExcepcionReportero("Mi loco, se daño la vuelta, no hicieron como a maduro y se cayo el sistema.", "Estadisticas");
+            }
+
+            OnFinalizado?.Invoke("Estadisticas listas.");
+            return "Visitas: 1500, Likes: 300, Shares: 75";
+        }
+
+       
 
     }
+
+
 
 
     // Clase principal del programa con Async y Await
