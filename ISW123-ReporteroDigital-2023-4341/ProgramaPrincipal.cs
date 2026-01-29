e144a5ff4e14410da08ab1a6bfcc213499d7f800
Luis Martin Peña Mejia <luismartinpm2580@gmail.com>
2026-01-28 22:24:25 -0400

    INICIO, CREACION DE EXCEPCION PERZONALIZADA, DESPUES DE UN RATO Y PAL DE ERRORES LOGRE QUE FUNCIONE

---
+++ ISW123-ReporteroDigital-2023-4341/ProgramaPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISW123_ReporteroDigital_2023_4341
+{
+
+    //Excepcion perzonalizada
+
+    public class ExcepcionReportero : Exception
+    { 
+        public string RecursoFallido { get; }
+        public ExcepcionReportero(string message, string recurso) : base(message)
+        {
+            RecursoFallido = recurso;
+        }
+    }
+
+
+    // Clase principal del programa con Async y Await
+    public class ProgramaPrincipal
+    {
+    }
+}
+
